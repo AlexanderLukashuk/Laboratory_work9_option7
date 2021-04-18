c52bb90e1f4ffc420de50563590e0bd9d7622fa6
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-18 21:10:51 +0600

    Сделана иницилизация массива через RandomDouble(), создана переменная count для подсчета элементов массива, которые больше первого элемента, создана переменная tempElem, которая хранит значение первого элемента массива

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace Task9v7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //* Задан массив, состоящий из 15 элементов вещественного типа. 
+            //* Определить количество элементов, значения которых больше первого элемента.
+
+            double[] numberArray = new double[15];
+            string input;
+            Random rand = new Random(DateTime.Now.Millisecond);
+            int count = 0;
+
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                /*Console.Write($"Введите {i + 1} число: ");
+                input = Console.ReadLine();
+                numberArray[i] = int.Parse(input);*/
+
+                numberArray[i] = Math.Round((rand.NextDouble() * 100), 5);
+                //Console.WriteLine($"{numberArray[i]}");
+            }
+
+            double tempElem = numberArray[0];
+
+            for (int i = 1; i < numberArray.Length; i++)
+            {
+                if (numberArray[i] > tempElem)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Количество элементов, которые больше {numberArray[0]} = {count}");
+
+
+            //* Дан массив вещественных чисел Z(16) . Определить разность между 
+            //* суммой элементов c четными индексами и суммой элементов, индексы 
+            //*которых кратны трем.
+
+
+            //* Ввести массив, состоящий из 15-ти элементов (двузначные целые числа). 
+            //* Изменить разрядность цифр, образующих элементы исходного массива и, 
+            //* таким образом, сформировать новый массив. Например, исходный
+            //* массив: 25 71 84…, новый массив: 52 17 48….
         }
     }
 }
